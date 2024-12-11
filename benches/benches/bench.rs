68b2f5f37df8d6273b020430655a511da1d5977c
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-11 20:20:05 +0000

    Make bench harness multi-year

--- benches/benches/bench.rs
+++ benches/benches/bench.rs
@@ -1,31 +1,55 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use paste::paste;
 
-macro_rules! benchmark {
-    ($name: ident) => {
-        paste::paste!{
-            static [<$name:upper _INPUT>]: &str = include_str!(concat!("../src/", stringify!($name), "/input.txt"));
+// Macro to generate benchmarks for a specific year
+macro_rules! benchmark_year {
+    ($year:ident, $($day:ident),+) => {
+        paste! {
+            $(
+                static [<$year:upper _ $day:upper _INPUT>]: &str = include_str!(
+                    concat!("../../", stringify!($year), "/src/", stringify!($day), "/input.txt")
+                );
 
-            fn $name(c: &mut Criterion) {
-                c.bench_function(concat!(stringify!($name), " Part 1"), |b| {
-                    b.iter(|| advent_2024::$name::part1(black_box([<$name:upper _INPUT>])));
-                });
-                c.bench_function(concat!(stringify!($name), " Part 2"), |b| {
-                    b.iter(|| advent_2024::$name::part2(black_box([<$name:upper _INPUT>])));
-                });
-            }
+                fn [<$year _ $day>](c: &mut Criterion) {
+                    c.bench_function(
+                        &format!("{} {} Part 1", stringify!($year), stringify!($day)), 
+                        |b| {
+                            b.iter(|| 
+                                $year::$day::part1(black_box([<$year:upper _ $day:upper _INPUT>]))
+                            );
+                        }
+                    );
+                    c.bench_function(
+                        &format!("{} {} Part 2", stringify!($year), stringify!($day)), 
+                        |b| {
+                            b.iter(|| 
+                                $year::$day::part2(black_box([<$year:upper _ $day:upper _INPUT>]))
+                            );
+                        }
+                    );
+                }
+            )+
+
+            // criterion_group!(
+            //     [<$year _benches>],
+            //     $([<$year _ $day>],)*
+            // );
         }
-    };
-}
-
-macro_rules! benchmarks {
-    ($($name:ident),+) => {
-        $(
-            benchmark!{$name}
-        )+
-
-        criterion_group!(benches, $($name,)+);
-        criterion_main!(benches);
     }
 }
 
-benchmarks! {day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11}
+macro_rules! benchmarks {
+    
+    ($year:ident, $($day:ident),+) => {
+        paste!{
+        $(
+            benchmark_year!{$year, $day}
+        )+
+
+        criterion_group!(benches, $([<$year _ $day>],)+);
+        criterion_main!(benches);
+    }
+    }
+}
+
+benchmarks! {y2024, day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11}
