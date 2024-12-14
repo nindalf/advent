f242299c04cbce88af95553fef548431dea35e5e
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-14 13:29:22 +0000

    Optimise 2023 day 1

--- y2023/src/day1/mod.rs
+++ y2023/src/day1/mod.rs
@@ -1,4 +1,4 @@
-use phf::{Map, phf_map};
+use phf::{phf_map, Map};
 
 const NUMBERS: Map<&str, u32> = phf_map! {
     "one" => 1,
@@ -23,6 +23,10 @@ const NUMBERS: Map<&str, u32> = phf_map! {
     "0" => 0,
 };
 
+/// Note on performance: The original version written in 2023 ran in 66.372 µs and 196.57 µs
+/// I tried a different algo for part 2 that was 4.35x slower. I didn't expect that.
+/// In any case I made a small change - multiplication + addition instead of string format + parse.
+/// This improved performance to 25.19 µs (-62%) and 159.05 µs (-19%).
 #[inline]
 pub fn part1(input: &str) -> u32 {
     input
@@ -30,34 +34,50 @@ pub fn part1(input: &str) -> u32 {
         .filter_map(|line| {
             let first = line.chars().find(|c| c.is_numeric())?;
             let last = line.chars().rev().find(|c| c.is_numeric())?;
-            let combined = format!("{first}{last}");
-            combined.parse::<u32>().ok()
+            Some(((first as u8 - b'0') * 10 + (last as u8 - b'0')) as u32)
         })
         .sum()
 }
 
 #[inline]
 pub fn part2(input: &str) -> u32 {
-    input
-        .lines()
-        .filter_map(|line| {
-            let mut numbers = Vec::with_capacity(10);
-            let mut remaining = line;
-            while !remaining.is_empty() {
-                for (k, v) in NUMBERS.entries() {
-                    if remaining.starts_with(k) {
-                        numbers.push(v);
-                        break;
-                    }
-                }
-                remaining = &remaining[1..];
+    input.lines().map(old).sum()
+}
+
+fn old(line: &str) -> u32 {
+    let mut numbers = Vec::with_capacity(10);
+    let mut remaining = line;
+    while !remaining.is_empty() {
+        for (k, v) in NUMBERS.entries() {
+            if remaining.starts_with(k) {
+                numbers.push(v);
+                break;
             }
-            let first = numbers[0];
-            let last = numbers[numbers.len() - 1];
-            let combined = format!("{first}{last}");
-            combined.parse::<u32>().ok()
-        })
-        .sum()
+        }
+        remaining = &remaining[1..];
+    }
+    let first = numbers[0];
+    let last = numbers[numbers.len() - 1];
+    first * 10 + last
+}
+
+#[allow(dead_code)]
+fn new(line: &str) -> u32 {
+    let (mut first, mut first_index) = (0, line.len());
+    let (mut last, mut last_index) = (0, 0);
+
+    for (k, v) in NUMBERS.entries() {
+        let matches: Vec<_> = line.match_indices(k).collect();
+        if !matches.is_empty() && matches[0].0 <= first_index {
+            first_index = matches[0].0;
+            first = *v;
+        }
+        if !matches.is_empty() && matches[matches.len() - 1].0 >= last_index {
+            last_index = matches[matches.len() - 1].0;
+            last = *v;
+        }
+    }
+    first * 10 + last
 }
 
 pub fn parse(_input: &str) {}
