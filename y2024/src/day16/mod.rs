b73573fa6517aaa010d63967ee3096dd6d5cb362
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-16 09:24:11 +0000

    Complete day 16 part 2

--- y2024/src/day16/mod.rs
+++ y2024/src/day16/mod.rs
@@ -3,37 +3,46 @@ use std::{
     collections::{hash_map::Entry, BinaryHeap},
 };
 
-use ahash::AHashMap;
+use ahash::{AHashMap, AHashSet};
 use common::grid::{Direction, Grid, Point};
 
 #[inline]
 pub fn part1(input: &str) -> u32 {
     let (grid, start) = parse(input);
-    dijkstra_path(grid, start, 'E')
+    dijkstra_path(grid, start, 'E').0
 }
 
 #[inline]
-pub fn part2(input: &str) -> u32 {
+pub fn part2(input: &str) -> usize {
     let (grid, start) = parse(input);
-    dijkstra_path(grid, start, 'E')
+    dijkstra_path(grid, start, 'E').1
 }
 
-fn dijkstra_path(grid: Grid<char>, start: Point, end_char: char) -> u32 {
+fn dijkstra_path(grid: Grid<char>, start: Point, end_char: char) -> (u32, usize) {
     let mut q = BinaryHeap::with_capacity(100);
     let mut seen: AHashMap<(usize, usize), u32> = AHashMap::with_capacity(grid.rows * grid.columns);
     let start_right = Path {
         point: start,
         cost: 0,
         direction: Direction::Right,
-        distance: 0,
+        covered: AHashSet::with_capacity(100),
     };
     q.push(start_right);
-    while let Some(path) = q.pop() {
-        // println!("Reached {:?} with cost {} while moving in direction {:?}", &path.point, path.cost, path.direction);
+    let mut paths = Vec::with_capacity(5);
+    while let Some(mut path) = q.pop() {
+        println!(
+            "Reached {:?} with cost {} while moving in direction {:?}",
+            &path.point, path.cost, path.direction
+        );
         match seen.entry(path.point) {
             Entry::Occupied(mut entry) => {
-                if *entry.get() <= path.cost {
-                    // println!("Quitting {:?} with cost {} because of previous cost {}", &path.point, path.cost, *entry.get());
+                if *entry.get() + 1000 < path.cost {
+                    println!(
+                        "Quitting {:?} with cost {} because of previous cost {}",
+                        &path.point,
+                        path.cost,
+                        *entry.get()
+                    );
                     // We've reached this point through a superior path
                     continue;
                 }
@@ -43,10 +52,13 @@ fn dijkstra_path(grid: Grid<char>, start: Point, end_char: char) -> u32 {
                 entry.insert(path.cost);
             }
         }
+        path.covered.insert(path.point);
+
         if let Some(c) = grid.get(path.point)
             && c == end_char
         {
-            return path.cost;
+            paths.push(path);
+            continue;
         }
 
         let [up, right, down, left] = grid.adjacent(path.point);
@@ -64,7 +76,7 @@ fn dijkstra_path(grid: Grid<char>, start: Point, end_char: char) -> u32 {
                 point: p,
                 cost: path.cost + cost,
                 direction: Direction::Up,
-                distance: path.distance + 1,
+                covered: path.covered.clone(),
             });
         }
         if let Some(p) = right
@@ -81,7 +93,7 @@ fn dijkstra_path(grid: Grid<char>, start: Point, end_char: char) -> u32 {
                 point: p,
                 cost: path.cost + cost,
                 direction: Direction::Right,
-                distance: path.distance + 1,
+                covered: path.covered.clone(),
             });
         }
         if let Some(p) = down
@@ -98,7 +110,7 @@ fn dijkstra_path(grid: Grid<char>, start: Point, end_char: char) -> u32 {
                 point: p,
                 cost: path.cost + cost,
                 direction: Direction::Down,
-                distance: path.distance + 1,
+                covered: path.covered.clone(),
             });
         }
         if let Some(p) = left
@@ -115,11 +127,18 @@ fn dijkstra_path(grid: Grid<char>, start: Point, end_char: char) -> u32 {
                 point: p,
                 cost: path.cost + cost,
                 direction: Direction::Left,
-                distance: path.distance + 1,
+                covered: path.covered.clone(),
             });
         }
     }
-    1024
+    let cost = paths[0].cost;
+    let mut all_good_points = AHashSet::with_capacity(100);
+    for path in paths.iter().filter(|path| path.cost == cost) {
+        for point in &path.covered {
+            all_good_points.insert(point);
+        }
+    }
+    (cost, all_good_points.len())
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -127,7 +146,7 @@ struct Path {
     point: Point,
     cost: u32,
     direction: Direction,
-    distance: u32,
+    covered: AHashSet<Point>,
 }
 
 impl Ord for Path {
@@ -148,4 +167,4 @@ fn parse(input: &str) -> (Grid<char>, Point) {
     (grid, start)
 }
 
-common::aoctest!(11048, 99460, 1234, 1234);
+common::aoctest!(11048, 99460, 64, 500);
