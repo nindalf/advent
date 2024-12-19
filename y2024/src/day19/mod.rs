15c4c98d0f1d6df5372a8676b434b93fe87dc149
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-19 17:04:36 +0000

    One more attempted optimisation for day 19

--- y2024/src/day19/mod.rs
+++ y2024/src/day19/mod.rs
@@ -9,9 +9,14 @@ use rayon::prelude::*;
 /// I replaced that with towels being a HashSet and checking the prefix of pattern is contained in the HashSet. Much faster
 /// Part 1 completed in 8.2ms single threaded and 1.2ms (-85%) while using rayon.
 /// I had to rewrite it with a recursive approach to make it work for part 2. This further optimised performance to 467µs (-61%).
+///
 /// Final numbers: Part 1 - 451µs, Part 2 - 454µs.
 /// They're both doing exactly the same computation but Part 2 takes slightly longer because it needs to add up the results.
 /// I could optimise Part 1 with a const generic param so it returns after just one match, but I prefer to keep the code looking simple and clean.
+///
+/// One more attempted optimisation - using a shared multi-threaded cache among all runs instead of a separate single-threaded cache for each one.
+/// The shared `&dashmap::DashMap` regresses performance by 130-140%. I guess for this input the amount of overlap between days
+/// isn't enough to justify the overhead of a multi-threaded map.
 #[inline]
 pub fn part1(input: &str) -> usize {
     let (towels, patterns) = parse(input);
@@ -37,22 +42,25 @@ pub fn part2(input: &str) -> u64 {
 fn match_towels_to_pattern<'a>(
     towels: &AHashSet<&str>,
     pattern: &'a str,
-    memo: &mut AHashMap<&'a str, u64>,
+    cache: &mut AHashMap<&'a str, u64>,
 ) -> u64 {
-    if memo.contains_key(pattern) {
-        return memo[pattern];
+    if cache.contains_key(pattern) {
+        return cache[pattern];
     }
+
     let mut matches = 0;
     for idx in 1..=8 {
         if idx <= pattern.len() && towels.contains(&pattern[0..idx]) {
             if idx == pattern.len() {
                 matches += 1;
-                continue;
+                break;
             }
-            matches += match_towels_to_pattern(towels, &pattern[idx..], memo);
+            matches += match_towels_to_pattern(towels, &pattern[idx..], cache);
         }
     }
-    *memo.entry(pattern).or_default() += matches;
+
+    *cache.entry(pattern).or_default() += matches;
+
     matches
 }
 
