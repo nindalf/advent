9ef8e6de02297baf20aa60a42fef6e1930a5409b
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-24 23:55:58 +0000

    Complete day 23

--- y2024/src/day23/Readme.md
+++ y2024/src/day23/Readme.md
@@ -82,8 +82,6 @@ Find all the sets of three inter-connected computers. *How many contain at least
 
 Your puzzle answer was `1368`.
 
-The first half of this puzzle is complete! It provides one gold star: \*
-
 \--- Part Two ---
 ----------
 
@@ -107,7 +105,11 @@ The LAN party posters say that the *password* to get into the LAN party is the n
 
 *What is the password to get into the LAN party?*
 
-Answer:
+Your puzzle answer was `dd,ig,il,im,kb,kr,pe,ti,tv,vr,we,xu,zi`.
 
-Although it hasn't changed, you can still [get your puzzle input](23/input).
+Both parts of this puzzle are complete! They provide two gold stars: \*\*
+
+At this point, you should [return to your Advent calendar](/2024) and try another puzzle.
+
+If you still want to see it, you can [get your puzzle input](23/input).
 
--- y2024/src/day23/mod.rs
+++ y2024/src/day23/mod.rs
@@ -1,5 +1,9 @@
 use ahash::{AHashMap, AHashSet};
 
+/// Shoutout to Wikipedia (https://en.wikipedia.org/wiki/Bron–Kerbosch_algorithm).
+/// First time I've heard of this problem in graph theory. Never even knew heard of "cliques" in computer science.
+/// Part 1 is fine - 379µs
+/// Part 2 is absurdly slow - 60 milliseconds. I can easily do better.
 #[inline]
 pub fn part1(input: &str) -> usize {
     let lan = parse(input);
@@ -22,19 +26,74 @@ pub fn part1(input: &str) -> usize {
 }
 
 #[inline]
-pub fn part2(_input: &str) -> i32 {
-    0
+pub fn part2(input: &str) -> String {
+    let lan = parse(input);
+    let r = AHashSet::new();
+    let p = lan.keys().copied().collect();
+    let x = AHashSet::new();
+    let maximal_clique = bron_kerbosch_clique_calculator(&lan, r, p, x);
+
+    let mut clique_vec: Vec<_> = maximal_clique.iter().copied().collect();
+    clique_vec.sort_unstable();
+    clique_vec.join(",")
 }
 
-fn parse(input: &str) -> AHashMap<&str, Vec<&str>> {
+fn bron_kerbosch_clique_calculator<'a>(
+    lan: &AHashMap<&str, AHashSet<&'a str>>,
+    r: AHashSet<&'a str>,
+    p: AHashSet<&'a str>,
+    x: AHashSet<&str>,
+) -> AHashSet<&'a str> {
+    if p.is_empty() && x.is_empty() {
+        return r;
+    }
+    // let (_, most_popular_node) = p.iter().map(|node| (lan[node].len(), node)).max().unwrap();
+    // println!("{:?}", most_popular_node);
+
+    let mut p_c = p.clone();
+    let mut x_c = x.clone();
+
+    let mut results = Vec::with_capacity(p.len());
+    for vertex in p.iter() {
+        let mut r_c = r.clone();
+
+        r_c.insert(vertex);
+        let neighbours = &lan[vertex];
+        let pruned_p = p_c.intersection(neighbours).copied().collect();
+        let pruned_x = x_c.intersection(neighbours).copied().collect();
+
+        let maximal_clique = bron_kerbosch_clique_calculator(lan, r_c, pruned_p, pruned_x);
+        results.push(maximal_clique);
+
+        p_c.remove(vertex);
+        x_c.insert(vertex);
+    }
+
+    if results.is_empty() {
+        return r;
+    }
+    results.sort_unstable_by_key(|a| a.len());
+    results[results.len() - 1].clone()
+}
+
+fn parse(input: &str) -> AHashMap<&str, AHashSet<&str>> {
     input
         .lines()
         .filter_map(|line| line.split_once("-"))
         .fold(AHashMap::new(), |mut acc, pair| {
-            acc.entry(pair.0).or_default().push(pair.1);
-            acc.entry(pair.1).or_default().push(pair.0);
+            acc.entry(pair.0)
+                .or_insert_with(|| AHashSet::with_capacity(15))
+                .insert(pair.1);
+            acc.entry(pair.1)
+                .or_insert_with(|| AHashSet::with_capacity(15))
+                .insert(pair.0);
             acc
         })
 }
 
-common::aoctest!(7, 1368, 1234, 1234);
+common::aoctest!(
+    7,
+    1368,
+    "co,de,ka,ta".to_owned(),
+    "dd,ig,il,im,kb,kr,pe,ti,tv,vr,we,xu,zi".to_owned()
+);
