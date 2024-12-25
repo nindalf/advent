59492ab53c703bd15f36c8127f26c6399cbf4950
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-25 12:12:18 +0000

    Optimise day 20

--- y2024/src/day20/mod.rs
+++ y2024/src/day20/mod.rs
@@ -1,6 +1,11 @@
-use ahash::{AHashMap, AHashSet};
+use ahash::AHashMap;
 use common::grid::{Grid, Point};
 
+/// Performance
+/// Part 1 - 473µs, not much to do here.
+/// Part 2 - 96.7ms initially. Optimised to 37.8ms (-61%) by replacing a HashSet with a simple counter and preventing double counting.
+/// I don't think there's much else I could do for part 2. Visiting every point and checking every other point within a euclid distance of 20
+/// is the bare minimum.
 #[inline]
 pub fn part1(input: &str) -> usize {
     let (grid, start) = parse(input);
@@ -55,29 +60,29 @@ fn num_cheats_20(path: AHashMap<Point, i32>) -> usize {
     let min_distance = if path.len() > 1000 { 100 } else { 50 };
     #[allow(unused_variables)]
     let mut results = 0;
-    let mut temp = AHashSet::with_capacity(path.len());
     for (point, path_distance) in path.iter() {
         for i in 0..21 {
             for j in 0..21 - i {
                 let euclid_distance = (i + j) as i32;
                 if let Some(other_distance) = path.get(&(point.0 + i, point.1 + j))
-                    && (path_distance - other_distance) - euclid_distance >= min_distance
+                    && path_distance - other_distance - euclid_distance >= min_distance
                 {
-                    temp.insert((*point, (point.0 + i, point.1 + j)));
                     results += 1;
                 }
                 if point.0 >= i
+                    && i != 0
+                    && j != 0
                     && let Some(other_distance) = path.get(&(point.0 - i, point.1 + j))
                     && path_distance - other_distance - euclid_distance >= min_distance
                 {
-                    temp.insert((*point, (point.0 - i, point.1 + j)));
                     results += 1;
                 }
                 if point.1 >= j
+                    && i != 0
+                    && j != 0
                     && let Some(other_distance) = path.get(&(point.0 + i, point.1 - j))
                     && path_distance - other_distance - euclid_distance >= min_distance
                 {
-                    temp.insert((*point, (point.0 + i, point.1 - j)));
                     results += 1;
                 }
                 if point.0 >= i
@@ -85,14 +90,13 @@ fn num_cheats_20(path: AHashMap<Point, i32>) -> usize {
                     && let Some(other_distance) = path.get(&(point.0 - i, point.1 - j))
                     && path_distance - other_distance - euclid_distance >= min_distance
                 {
-                    temp.insert((*point, (point.0 - i, point.1 - j)));
                     results += 1;
                 }
             }
         }
     }
 
-    temp.len()
+    results
 }
 
 fn parse(input: &str) -> (Grid<char>, Point) {
