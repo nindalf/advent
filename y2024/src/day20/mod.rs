194b15e7726cc2a8ff845f854d08f35bc5c9b6dc
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-25 17:20:43 +0000

    Fail to optimise day 20

--- y2024/src/day20/mod.rs
+++ y2024/src/day20/mod.rs
@@ -4,8 +4,11 @@ use common::grid::{Grid, Point};
 /// Performance
 /// Part 1 - 473µs, not much to do here.
 /// Part 2 - 96.7ms initially. Optimised to 37.8ms (-61%) by replacing a HashSet with a simple counter and preventing double counting.
-/// I don't think there's much else I could do for part 2. Visiting every point and checking every other point within a euclid distance of 20
-/// is the bare minimum.
+/// Alternate solution: store the path in a Vec, where the index is the distance from the beginning.
+/// But it doesn't perform better for either part because the number of lookups is much higher. Cache-friendliness doesn't compensate for that.
+/// The code is much cleaner though, so I kept it.
+/// Part 1 with Vec takes 59.2ms (+12399% regression).
+/// Part 2 with Vec is 59.4ms (+57 regression).
 #[inline]
 pub fn part1(input: &str) -> usize {
     let (grid, start) = parse(input);
@@ -103,6 +106,48 @@ fn parse(input: &str) -> (Grid<char>, Point) {
     let grid = Grid::construct(input, |x| x);
     let start = grid.search('S').expect("grid has a start");
     (grid, start)
+}
+
+#[allow(dead_code)]
+fn dfs_path_vec(grid: &Grid<char>, start: Point) -> Vec<Point> {
+    let mut path = Vec::with_capacity(grid.rows * grid.columns);
+    path.push(start);
+    let mut current = start;
+    loop {
+        for point_with_val in grid.adjacent_one(current) {
+            if let (Some(p), Some('.')) = point_with_val
+                && (path.len() < 2 || path[path.len() - 2] != p)
+            {
+                path.push(p);
+                current = p;
+                break;
+            }
+            if let (Some(p), Some('E')) = point_with_val {
+                path.push(p);
+                return path;
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn num_cheats_vec(path: Vec<Point>, max_cheat_distance: usize, min_distance: usize) -> usize {
+    let mut results = 0;
+    for (idx_start, point) in path.iter().enumerate() {
+        if idx_start + min_distance >= path.len() {
+            break;
+        }
+        for (idx_end, other_point) in path.iter().enumerate().skip(idx_start + min_distance + 1) {
+            let euclid_distance = common::grid::euclid_distance(point, other_point);
+            if euclid_distance <= max_cheat_distance
+                && idx_end - idx_start > euclid_distance
+                && idx_end - idx_start - euclid_distance >= min_distance
+            {
+                results += 1
+            }
+        }
+    }
+    results
 }
 
 common::aoctest!(44, 1499, 285, 1027164);
