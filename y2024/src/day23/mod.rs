b7ec4cea21c56c478f027b4be35c4462cec80dd2
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-26 10:30:28 +0000

    Optimise day 23

--- y2024/src/day23/mod.rs
+++ y2024/src/day23/mod.rs
@@ -4,6 +4,10 @@ use ahash::{AHashMap, AHashSet};
 /// First time I've heard of this problem in graph theory. Never even knew heard of "cliques" in computer science.
 /// Part 1 is fine - 379µs
 /// Part 2 is absurdly slow - 60 milliseconds. I can easily do better.
+///
+/// Performance
+/// 1. Remove set X. In this case it didn't seem to do anythin. Part 2 - 44.1ms (-54.1%).
+/// 2. I should be able to use the more performant version of Bron-Kerbosch but I wasn't able to make it work.
 #[inline]
 pub fn part1(input: &str) -> usize {
     let lan = parse(input);
@@ -30,8 +34,7 @@ pub fn part2(input: &str) -> String {
     let lan = parse(input);
     let r = AHashSet::new();
     let p = lan.keys().copied().collect();
-    let x = AHashSet::new();
-    let maximal_clique = bron_kerbosch_clique_calculator(&lan, r, p, x);
+    let maximal_clique = bron_kerbosch_clique_calculator(&lan, r, p);
 
     let mut clique_vec: Vec<_> = maximal_clique.iter().copied().collect();
     clique_vec.sort_unstable();
@@ -42,16 +45,11 @@ fn bron_kerbosch_clique_calculator<'a>(
     lan: &AHashMap<&str, AHashSet<&'a str>>,
     r: AHashSet<&'a str>,
     p: AHashSet<&'a str>,
-    x: AHashSet<&str>,
 ) -> AHashSet<&'a str> {
-    if p.is_empty() && x.is_empty() {
+    if p.is_empty() {
         return r;
     }
-    // let (_, most_popular_node) = p.iter().map(|node| (lan[node].len(), node)).max().unwrap();
-    // println!("{:?}", most_popular_node);
-
     let mut p_c = p.clone();
-    let mut x_c = x.clone();
 
     let mut results = Vec::with_capacity(p.len());
     for vertex in p.iter() {
@@ -60,13 +58,11 @@ fn bron_kerbosch_clique_calculator<'a>(
         r_c.insert(vertex);
         let neighbours = &lan[vertex];
         let pruned_p = p_c.intersection(neighbours).copied().collect();
-        let pruned_x = x_c.intersection(neighbours).copied().collect();
 
-        let maximal_clique = bron_kerbosch_clique_calculator(lan, r_c, pruned_p, pruned_x);
+        let maximal_clique = bron_kerbosch_clique_calculator(lan, r_c, pruned_p);
         results.push(maximal_clique);
 
         p_c.remove(vertex);
-        x_c.insert(vertex);
     }
 
     if results.is_empty() {
