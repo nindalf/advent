0e81191541bfd0599228129d135b06975e3e6fd3
Krishna Sundarram <krishnasundarram@monzo.com>
2024-12-27 18:55:56 +0000

    Complete day 21 part 2

--- y2024/src/day21/mod.rs
+++ y2024/src/day21/mod.rs
@@ -6,15 +6,7 @@ pub fn part1(input: &str) -> u64 {
         .map(|keypad| {
             let code = keypad.code;
             let x = "A".to_string() + &keypad.collect::<String>();
-            let y = "A".to_string()
-                + &x.chars()
-                    .map_windows(|[x, y]| next_dpad_movement(*x, *y))
-                    .collect::<String>();
-            let z = y
-                .chars()
-                .map_windows(|[x, y]| next_dpad_movement(*x, *y))
-                .collect::<String>();
-            z.len() as u64 * code
+            num_dpad_movements(x, 1) * code
         })
         .sum()
 }
@@ -25,32 +17,86 @@ pub fn part2(input: &str) -> u64 {
         .map(|keypad| {
             let code = keypad.code;
             let x = "A".to_string() + &keypad.collect::<String>();
-            let mut result: AHashMap<&str, u64> = x
-                .chars()
-                .map_windows(|[x, y]| next_dpad_movement(*x, *y))
-                .fold(AHashMap::with_capacity(15), |mut acc, movement| {
-                    *acc.entry(movement).or_default() += 1;
-                    acc
-                });
-            for _ in 0..26 {
-                let mut temp = AHashMap::with_capacity(15);
-                for (movement, n) in result {
-                    for next_movement in next_dpad_chunk(movement) {
-                        if next_movement.is_empty() {
-                            continue;
-                        }
-                        *temp.entry(next_movement).or_default() += n
-                    }
-                }
-                result = temp;
-            }
-            let sum: u64 = result
-                .iter()
-                .map(|(movement, n)| (movement.len() as u64) * n)
-                .sum();
-            sum * code
+            num_dpad_movements(x, 25) * code
         })
         .sum()
+}
+
+fn num_dpad_movements(initial: String, num_dpad_robots: u64) -> u64 {
+    let mut result: AHashMap<&str, u64> = initial
+        .chars()
+        .map_windows(|[x, y]| next_dpad_movement(*x, *y))
+        .fold(AHashMap::with_capacity(15), |mut acc, movement| {
+            *acc.entry(movement).or_default() += 1;
+            acc
+        });
+
+    for _ in 0..num_dpad_robots {
+        let mut temp = AHashMap::with_capacity(15);
+        for (movement, n) in result {
+            for next_movement in next_dpad_chunk(movement) {
+                if next_movement.is_empty() {
+                    break;
+                }
+                *temp.entry(next_movement).or_default() += n
+            }
+        }
+        result = temp;
+    }
+
+    result
+        .iter()
+        .map(|(movement, n)| (movement.len() as u64) * n)
+        .sum()
+}
+
+fn next_dpad_movement(current: char, destination: char) -> &'static str {
+    match (current, destination) {
+        ('^', '^') => "A",
+        ('^', '>') => ">vA",
+        ('^', '<') => "v<A",
+        ('^', 'A') => ">A",
+        ('>', '^') => "<^A",
+        ('>', '>') => "A",
+        ('>', 'v') => "<A",
+        ('>', 'A') => "^A",
+        ('v', '>') => ">A",
+        ('v', 'v') => "A",
+        ('v', '<') => "<A",
+        ('v', 'A') => ">^A",
+        ('<', '^') => ">^A",
+        ('<', 'v') => ">A",
+        ('<', '<') => "A",
+        ('<', 'A') => ">>^A",
+        ('A', '^') => "<A",
+        ('A', '>') => "vA",
+        ('A', 'v') => "<vA",
+        ('A', '<') => "v<<A",
+        ('A', 'A') => "A",
+        // ('^', 'v') => "vA", // Should never happen
+        // ('>', '<') => "<<A", // Should never happen
+        // ('v', '^') => "^A", // Should never happen
+        // ('<', '>') => ">>A", // Should never happen
+        _ => unreachable!("All cases handled"),
+    }
+}
+
+fn next_dpad_chunk(movements: &str) -> [&'static str; 4] {
+    match movements {
+        "^A" => ["<A", ">A", "", ""],
+        "<^A" => ["v<<A", ">^A", ">A", ""],
+        "<A" => ["v<<A", ">>^A", "", ""],
+        "<vA" => ["v<<A", ">A", ">^A", ""],
+        ">^A" => ["vA", "<^A", ">A", ""],
+        ">>^A" => ["vA", "A", "<^A", ">A"],
+        ">A" => ["vA", "^A", "", ""],
+        ">vA" => ["vA", "<A", ">^A", ""],
+        "A" => ["A", "", "", ""],
+        "v<<A" => ["<vA", "<A", "A", ">>^A"],
+        "v<A" => ["<vA", "<A", ">>^A", ""],
+        "vA" => ["<vA", ">^A", "", ""],
+        _ => unreachable!("Unrecognised movement"),
+    }
 }
 
 fn parse(input: &str) -> impl Iterator<Item = KeyPad> + use<'_> {
@@ -128,55 +174,6 @@ impl Iterator for KeyPad {
     }
 }
 
-fn next_dpad_movement(current: char, destination: char) -> &'static str {
-    match (current, destination) {
-        ('^', '^') => "A",
-        ('^', '>') => ">vA",
-        ('^', '<') => "v<A",
-        ('^', 'A') => ">A",
-        ('>', '^') => "<^A",
-        ('>', '>') => "A",
-        ('>', 'v') => "<A",
-        ('>', 'A') => "^A",
-        ('v', '>') => ">A",
-        ('v', 'v') => "A",
-        ('v', '<') => "<A",
-        ('v', 'A') => ">^A",
-        ('<', '^') => ">^A",
-        ('<', 'v') => ">A",
-        ('<', '<') => "A",
-        ('<', 'A') => ">>^A",
-        ('A', '^') => "<A",
-        ('A', '>') => "vA",
-        ('A', 'v') => "<vA",
-        ('A', '<') => "v<<A",
-        ('A', 'A') => "A",
-        // ('^', 'v') => "vA", // Should never happen
-        // ('>', '<') => "<<A", // Should never happen
-        // ('v', '^') => "^A", // Should never happen
-        // ('<', '>') => ">>A", // Should never happen
-        _ => unreachable!("All cases handled"),
-    }
-}
-
-fn next_dpad_chunk(movements: &str) -> [&'static str; 4] {
-    match movements {
-        "^A" => ["<A", ">A", "", ""],
-        "<^A" => ["v<<A", ">^A", ">A", ""],
-        "<A" => ["v<<A", ">>^A", "", ""],
-        "<vA" => ["v<<A", ">A", ">^A", ""],
-        ">^A" => ["vA", "<^A", ">A", ""],
-        ">>^A" => ["vA", "A", "<^A", ">A"],
-        ">A" => ["vA", "^A", "", ""],
-        ">vA" => ["vA", "<A", ">^A", ""],
-        "A" => ["A", "", "", ""],
-        "v<<A" => ["<vA", "<A", "A", ">>^A"],
-        "v<A" => ["<vA", "<A", ">>^A", ""],
-        "vA" => ["<vA", ">^A", "", ""],
-        _ => unreachable!("Unrecognised movement"),
-    }
-}
-
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 enum NumPadKey {
     Zero,
@@ -230,4 +227,4 @@ impl NumPadKey {
     }
 }
 
-common::aoctest!(126384, 188398, 1234, 230049027535970);
+common::aoctest!(126384, 188398, 154115708116294, 230049027535970);
