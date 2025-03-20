b72b98b5eeb4615a6f34c1c470538a83f77732f6
Krishna Sundarram <krishnasundarram@monzo.com>
2025-03-20 17:46:58 +0000

    Rewrite macro with crabtime

--- benches/benches/bench.rs
+++ benches/benches/bench.rs
@@ -1,67 +1,84 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use paste::paste;
 
-// Macro to generate benchmarks for a specific year
-macro_rules! benchmark_year {
-    ($year:ident, $($day:ident),+) => {
-        paste! {
-            $(
-                static [<$year:upper _ $day:upper _INPUT>]: &str = include_str!(
-                    concat!("../../", stringify!($year), "/src/", stringify!($day), "/input.txt")
+#[crabtime::function]
+fn gen_benches() {
+    // Find all solution modules
+    // Search the project root for crates that match the pattern y20*/src/day*/mod.rs
+    // Return a list of tuples containing the year and day of each solution module
+    let project_root = env!("CARGO_MANIFEST_DIR");
+    // This relative path is brittle and will break if implementation details in crabtime change
+    // Ideally crabtime would set an environment variable pointing to the project root
+    let pattern = format!(
+        "{}/{}",
+        project_root, "../../../../../../../../y20*/src/day*/mod.rs"
+    );
+    let mut years_and_days = glob::glob(&pattern)
+        .unwrap()
+        .filter_map(|entry| entry.ok())
+        .filter_map(|path| {
+            let components = path
+                .components()
+                .map(|c| c.as_os_str())
+                .filter_map(|c| c.to_str())
+                .map(|c| c.to_string())
+                .collect::<Vec<String>>();
+            let year = components[components.len() - 4].clone();
+            let day = components[components.len() - 2].clone();
+            Some((year, day))
+        })
+        .collect::<Vec<(String, String)>>();
+
+    // Sort the list so "day2" appears before "day10"
+    years_and_days.sort_by(|a, b| match a.1.len().cmp(&b.1.len()) {
+        std::cmp::Ordering::Equal => a.1.cmp(&b.1),
+        ordering => ordering,
+    });
+
+    // For every year and day ("y2024", "day3"), generate a benchmark function
+    // Store the benchmark function names in a vector and pass them to the criterion_group macro
+    let mut fn_names = Vec::new();
+    for (year, day) in years_and_days {
+        let input_file_path = &format!("../../{}/src/{}/input.txt", year, day);
+        let input_file = format!("{}_{}_INPUT", year, day);
+
+        let fn_name = format!("{}_{}", year, day);
+        fn_names.push(fn_name.clone());
+
+        let module_name = format!("{}::{}", year, day);
+
+        let test_str_one = format!("{} {} Part 1", year, day);
+        let test_str_two = format!("{} {} Part 2", year, day);
+
+        crabtime::output! {
+            static {{input_file}}: &str = include_str!(stringify!({{input_file_path}}));
+            fn {{fn_name}}(c: &mut Criterion) {
+                c.bench_function(
+                    stringify!({{test_str_one}}),
+                    |b| {
+                        b.iter(||
+                            {{module_name}}::part1(black_box({{input_file}}))
+                        );
+                    }
                 );
-
-                fn [<$year _ $day>](c: &mut Criterion) {
-                    c.bench_function(
-                        &format!("{} {} Part 1", stringify!($year), stringify!($day)),
-                        |b| {
-                            b.iter(||
-                                $year::$day::part1(black_box([<$year:upper _ $day:upper _INPUT>]))
-                            );
-                        }
-                    );
-                    c.bench_function(
-                        &format!("{} {} Part 2", stringify!($year), stringify!($day)),
-                        |b| {
-                            b.iter(||
-                                $year::$day::part2(black_box([<$year:upper _ $day:upper _INPUT>]))
-                            );
-                        }
-                    );
-                }
-            )+
+                c.bench_function(
+                    stringify!({{test_str_two}}),
+                    |b| {
+                        b.iter(||
+                            {{module_name}}::part2(black_box({{input_file}}))
+                        );
+                    }
+                );
+            }
         }
+    }
+    let fn_names = fn_names.join(", ");
+    crabtime::output! {
+        criterion_group!(benches, {{fn_names}});
+        criterion_main!(benches);
     }
 }
 
-macro_rules! benchmarks {
-    // Match one or more year-days groups
-    ($($year:ident {$($day:ident),+ $(,)?}),+ $(,)?) => {
-        paste! {
-            // For each year-day combination, generate the benchmark
-            $(
-                $(
-                    benchmark_year!{$year, $day}
-                )+
-            )+
+// I don't know why rust-analyzer says "Compilation of the generated code failed."
+// rustc compiles it.
 
-            // Create a single criterion group with all benchmarks
-            criterion_group!(
-                benches,
-                $(
-                    $(
-                        [<$year _ $day>],
-                    )+
-                )+
-            );
-            criterion_main!(benches);
-        }
-    };
-}
-
-benchmarks! {
-    y2023 {day1, day2},
-    y2024 {
-        day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13,
-        day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-    },
-}
+gen_benches!();
